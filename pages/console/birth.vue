6f87a8d3bce22cd180a0028ff05440f6655e0205
Kelvin Omereshone <kelvinomereshone@gmail.com>
2019-11-11 15:43:37 +0100

    feat: added feature for updating a record

--- pages/console/birth.vue
+++ pages/console/birth.vue
@@ -18,6 +18,12 @@
         v-if="recordFetchingState === 'SUCCESS' && record.id"
         class="main-card record"
       >
+        <div
+          v-if="isLoggedIn && wavesKeeperData.publicKey === record.issuer"
+          class="edit-wrapper"
+        >
+          <i class="material-icons edit-icon" @click="openEditModal">edit</i>
+        </div>
         <div class="record-body">
           <h1 class="name">{{ record.info.name | uppercase }}</h1>
           <p class="date-of-birth">{{ record.info.dateOfBirth }}</p>
@@ -43,6 +49,74 @@
         </footer>
       </div>
     </transition>
+    <modal
+      name="editRecordModal"
+      :adaptive="true"
+      :draggable="true"
+      :scrollable="true"
+      height="auto"
+    >
+      <form class="add-new-record-form" @submit.enter.prevent="updateRecord">
+        <div v-if="recordAdded" class="notification">
+          Record updated
+          <a
+            target="_blank"
+            class="notification__link"
+            :href="
+              `https://wavesexplorer.com/testnet/tx/${transaction.info.id}`
+            "
+            >{{ transaction.info.trace[0].result.data[0].key }}</a
+          >
+        </div>
+        <div class="form-group">
+          <input v-model="name" type="text" placeholder="Full name" />
+        </div>
+        <div class="form-group">
+          <input
+            v-model="dateOfBirth"
+            type="text"
+            placeholder="Date of birth e.g DD/MM/YYYY"
+          />
+        </div>
+        <div class="form-group">
+          <input
+            v-model="lga"
+            type="text"
+            placeholder="Local Government Area"
+          />
+        </div>
+        <div class="form-group">
+          <div class="radio">
+            <input
+              id="male"
+              v-model="sex"
+              type="radio"
+              value="male"
+              name="sex"
+              :checked="record.info.sex === 'female'"
+            />
+            <label for="male">Male</label>
+            <div class="check"><div class="inside"></div></div>
+          </div>
+          <div class="radio">
+            <input
+              id="female"
+              v-model="sex"
+              type="radio"
+              value="female"
+              name="sex"
+              :checked="record.info.sex === 'female'"
+            />
+            <label for="female">Female</label>
+            <div class="check"><div class="inside"></div></div>
+          </div>
+        </div>
+        <button type="submit" class="primary-button" :disabled="isAddingRecord">
+          <i v-if="isAddingRecord" class="material-icons">watch_later</i>
+          <i v-else class="material-icons">edit</i>
+        </button>
+      </form>
+    </modal>
   </div>
 </template>
 
@@ -53,11 +127,37 @@ export default {
   middleware: 'isAuthenticated',
   data() {
     return {
-      recordId: ''
+      recordId: '',
+      name: '',
+      dateOfBirth: '',
+      sex: '',
+      lga: '',
+      isAddingRecord: false,
+      recordAdded: false
     }
   },
   computed: {
-    ...mapState(['recordFetchingState', 'record'])
+    ...mapState([
+      'recordFetchingState',
+      'record',
+      'isLoggedIn',
+      'userStatus',
+      'wavesKeeperData',
+      'dAppAddress'
+    ]),
+    isWhitelisted() {
+      let bool = false
+      switch (this.userStatus) {
+        case 'whitelisted':
+          bool = true
+          break
+        case 'blacklisted':
+        case 'notlisted':
+          bool = false
+          break
+      }
+      return bool
+    }
   },
   watch: {
     recordFetchingState(state) {
@@ -77,7 +177,91 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['searchForRecordById'])
+    ...mapActions(['searchForRecordById']),
+    populateCurrentRecord() {
+      this.name = this.record.info.name
+      this.lga = this.record.info.lga
+      this.dateOfBirth = this.record.info.dateOfBirth
+      if (this.record.info.sex === 'female') {
+        this.sex = 'female'
+      } else if (this.record.info.sex === 'male') {
+        this.sex = 'male'
+      }
+    },
+    openEditModal() {
+      this.$modal.show('editRecordModal')
+      this.populateCurrentRecord()
+    },
+    updateRecord() {
+      this.isAddingRecord = true
+      this.recordAdded = false
+      this.$snack.success({
+        text: 'Adding new record to Waves Blockchain'
+      })
+      const info = {
+        name: this.name,
+        sex: this.sex,
+        dateOfBirth: this.dateOfBirth,
+        lga: this.lga
+      }
+
+      console.log(info)
+      // eslint-disable-next-line no-unreachable
+      const payload = JSON.stringify(info)
+      const tx = {
+        type: 16,
+        data: {
+          dApp: this.dAppAddress,
+          call: {
+            function: 'updateRecord',
+            args: [
+              { type: 'string', value: this.record.id },
+              { type: 'string', value: payload }
+            ]
+          },
+          payment: [],
+          fee: {
+            assetId: 'WAVES',
+            amount: '500000'
+          }
+        }
+      }
+
+      try {
+        // eslint-disable-next-line no-undef
+        WavesKeeper.signAndPublishTransaction(tx)
+          .then((data) => {
+            this.isAddingRecord = false
+            this.$snack.success({
+              text: '👍 Record added successfully'
+            })
+            this.recordAdded = true
+            const result = {
+              info: JSON.parse(data)
+            }
+            console.log(result.info)
+            this.transaction = result
+            this.$modal.hide('editRecordModal')
+            this.$router.push({ path: '/console/records' })
+          })
+          .catch((error) => {
+            console.log(error)
+            this.$snack.success({
+              text:
+                '😰 Oops this is embarrasing something went wrong. Try again'
+            })
+
+            this.isAddingRecord = false
+            this.recordAdded = false
+          })
+      } catch (_) {
+        this.isAddingRecord = false
+        this.recordAdded = false
+        this.$snack.success({
+          text: '😰 Oops this is embarrasing something went wrong. Try again'
+        })
+      }
+    }
   }
 }
 </script>
@@ -131,5 +315,17 @@ export default {
 .certificate--leave-active {
   opacity: 1;
   transition: all 0.3s;
+}
+
+.edit-wrapper {
+  display: flex;
+  justify-content: flex-end;
+}
+.edit-icon {
+  position: flex;
+  color: var(--primary-color);
+  cursor: pointer;
+  justify-content: flex-end;
+  align-self: flex-end;
 }
 </style>
